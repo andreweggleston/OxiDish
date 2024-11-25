0b5a0ca26a5ce83349bdb51c21d112ca6e75bc70
Andrew Eggleston <egglestonandrew927@gmail.com>
2024-11-25 13:28:29 -0500

    added new database test: - querying a recipe id which does not exist

--- database/tests/queries.rs
+++ database/tests/queries.rs
@@ -1,29 +1,29 @@
-use database::queries::recipe::{self, RecipeResponse};
+use database::queries::recipe::get_recipe_by_id;
 
-#[sqlx::test(fixtures("ingredient_names", "recipes"))]
-async fn test_get_recipe_by_id(pool: sqlx::PgPool) -> sqlx::Result<()> {
-    let recipe = recipe::get_recipe_by_id(&pool, 1).await?;
+#[sqlx::test(fixtures("fixtures/ingredient_names.sql", "fixtures/recipes.sql"))]
+async fn test_get_recipe_by_id_where_id_exists(pool: sqlx::PgPool) -> sqlx::Result<()> {
+    let recipe = get_recipe_by_id(&pool, 1).await?;
     assert!(recipe.is_some());
     let recipe = recipe.unwrap();
-    println!(
-        r#"
-        id: {}
-        title: {}
-        description: {}"#,
-        recipe.id, recipe.title, recipe.description
-    );
+    assert_eq!(recipe.id, 1);
+    assert_eq!(recipe.title, "Pasta Bolognese");
+    assert_eq!(recipe.description, "A simple pasta sauce");
 
-    println!("ingredients:");
-    for i in &recipe.ingredients {
-        println!(
-            "\tid_name: {}\n\tname: {}\n\tamount: {}\n",
-            i.id_name, i.name, i.amount
-        );
-    }
+    assert!(recipe
+        .ingredients
+        .iter()
+        .any(|ingredient| { ingredient.name == "ground beef" }));
+    assert!(recipe
+        .ingredients
+        .iter()
+        .any(|ingredient| { ingredient.name == "tomato sauce" }));
+    Ok(())
+}
 
-    println!();
-
-    todo!();
-
+#[sqlx::test(fixtures("fixtures/ingredient_names.sql", "fixtures/recipes.sql"))]
+async fn test_get_recipe_by_id_where_id_not_exists(pool: sqlx::PgPool) -> sqlx::Result<()> {
+    let recipe = get_recipe_by_id(&pool, 55224).await?; // nonsense id
+                                                        // number
+    assert!(recipe.is_none());
     Ok(())
 }
